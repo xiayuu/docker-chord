6235c00035abadca3bd80119351fdc8936fc69e2
Your Name <you@example.com>
2017-08-05 14:47:17 +0800

    bug fix

--- build.sh
+++ build.sh
@@ -4,5 +4,5 @@ mkdir build
 git clone https://github.com/xiayuu/p2pviewer.git build/p2pviewer
 git clone https://github.com/xiayuu/rpcudp.git build/rpcudp
 git clone https://github.com/xiayuu/chord.git build/chord
-#docker build -t p2pviewer -f p2pviewer.dockerfile .
+docker build -t p2pviewer -f p2pviewer.dockerfile .
 docker build -t chord -f chord.dockerfile .
--- chord.dockerfile
+++ chord.dockerfile
@@ -4,6 +4,8 @@ WORKDIR /app
 
 add ./build/rpcudp/ /app/rpcudp
 add ./build/chord/ /app/chord
+add runbootnode.sh /app/
+add runnodes.sh /app/
 RUN apt update && apt install -y gcc
 RUN pip install msgpack-python
 RUN cd /app/rpcudp/; python setup.py install
